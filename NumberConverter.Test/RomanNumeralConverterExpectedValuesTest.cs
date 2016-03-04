9248d0187c2183c2135d169c4745db63cee1929b
CuongDuongDuy <cuong.duongduy@gmail.com>
2016-03-04 10:45:16 +0000

    #Initialize

---
+++ NumberConverter.Test/RomanNumeralConverterExpectedValuesTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumberConverter.Test
+{
+    [TestClass]
+    public class RomanNumeralConverterExpectedValuesTest
+    {
+        [TestMethod]
+        public void Number_3000_Shoud_Return_MMM()
+        {
+            var converter = new RomanNumeralConverter();
+            var letter = converter.CovertToLetter(3000);
+            Assert.AreEqual("MMM", letter);
+        }
+
+        [TestMethod]
+        public void Number_1000_Should_Return_M()
+        {
+            var converter = new RomanNumeralConverter();
+            var letter = converter.CovertToLetter(1000);
+            Assert.AreEqual("M", letter);
+        }
+
+
+        [TestMethod]
+        public void Number_550_Should_Return_DL()
+        {
+            var converter = new RomanNumeralConverter();
+            var letter = converter.CovertToLetter(550);
+            Assert.AreEqual("DL", letter);
+        }
+
+        [TestMethod]
+        public void Number_200_Should_Return_CC()
+        {
+            var converter = new RomanNumeralConverter();
+            var letter = converter.CovertToLetter(200);
+            Assert.AreEqual("CC", letter);
+        }
+
+        [TestMethod]
+        public void Number_10_Should_Return_X()
+        {
+            var converter = new RomanNumeralConverter();
+            var letter = converter.CovertToLetter(10);
+            Assert.AreEqual("X", letter);
+        }
+
+        [TestMethod]
+        public void Number_1_Should_Return_I()
+        {
+            var converter = new RomanNumeralConverter();
+            var letter = converter.CovertToLetter(1);
+            Assert.AreEqual("I", letter);
+        }
+
+    }
+}
---
+++ NumberConverter.Test/RomanNumeralConverterOutOfIndexTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumberConverter.Test
+{
+    [TestClass]
+    public class RomanNumeralConverterOutOfIndexTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NumberGreaterThan3000ThrowsIndexOutOfRangeException()
+        {
+            var converter = new RomanNumeralConverter();
+            converter.CovertToLetter(3001);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NumberLessThan1ThrowsIndexOutOfRangeException()
+        {
+            var converter = new RomanNumeralConverter();
+            converter.CovertToLetter(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NumberMinus1ThrowsIndexOutOfRangeException()
+        {
+            var converter = new RomanNumeralConverter();
+            converter.CovertToLetter(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Number3002ThrowsIndexOutOfRangeException()
+        {
+            var converter = new RomanNumeralConverter();
+            converter.CovertToLetter(3002);
+        }
+    }
+
+   
+}
